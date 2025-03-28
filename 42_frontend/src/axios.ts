68c45e97e7d705665f2cdc33a56d8797fee31b65
s5hyeonkim <zxas2596@gmail.com>
2025-03-28 18:54:09 +0900

    navigate on

--- 42_frontend/src/axios.ts
+++ 42_frontend/src/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 // const sessionId =
 //   sessionStorage.getItem("sessionid") || localStorage.getItem("sessionid");
+const navigate = useNavigate();
 
 export const $ = axios.create({
   baseURL: "http://localhost:2424",
@@ -29,7 +31,8 @@ $.interceptors.response.use(
         window.location.href = `http://localhost:2424/main`;
       });
     } else if (error.response.data.redirect.redirect_url) {
-      window.location.href = `http://localhost:2424${error.response.data.redirect.redirect_url}`;
+      // window.location.href = `http://localhost:2424${error.response.data.redirect.redirect_url}`;
+      navigate(error.response.data.redirect.redirect_url, { replace: true });
     }
     return Promise.reject(error);
   }
