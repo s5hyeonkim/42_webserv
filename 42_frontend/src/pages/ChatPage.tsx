a0b9cf5e1e26775276d3c8b097d7f3f6e0862bc0
s5hyeonkim <zxas2596@gmail.com>
2025-04-02 12:09:21 +0900

    chatting room back 금지 추가

--- 42_frontend/src/pages/ChatPage.tsx
+++ 42_frontend/src/pages/ChatPage.tsx
@@ -6,11 +6,28 @@ import ChatDisplay from "./components/ChatDisplay.tsx";
 // import FileDownload from "./components/FileDownload.tsx";
 import { ToastContainer } from "react-toastify";
 import Input from "./components/Input.tsx";
+import { useEffect } from "react";
 
 const ChatPage = () => {
   console.log("this is chat page");
+  useEffect(() => {
+    const preventBack = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      // e.returnValue = '';
+    };
+    const handlePopState = () => {
+      window.history.pushState(null, "", window.location.href);
+    };
+    window.history.pushState(null, "", window.location.href);
+    window.addEventListener("popstate", handlePopState);
+    window.addEventListener("beforeunload", preventBack);
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+      window.removeEventListener("beforeunload", preventBack);
+    };
+  });
   return (
-    <div style ={{padding: "20px"}}>
+    <div style={{ padding: "20px" }}>
       <Outlet />
       <ToastContainer />
       <ChatDisplay />
