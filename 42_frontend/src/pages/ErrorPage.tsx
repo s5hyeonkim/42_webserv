38843f7e56adfb03d6b0c5c4e430002e17bca0fa
s5hyeonkim <zxas2596@gmail.com>
2025-04-02 10:47:05 +0900

    뒤로가기 금지 추가

--- 42_frontend/src/pages/ErrorPage.tsx
+++ 42_frontend/src/pages/ErrorPage.tsx
@@ -1,7 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { $ } from "../axios";
 
 const ErrorPage = () => {
   console.log("default error page");
+  const navigate = useNavigate();
+  const whatError = async () => {
+    const response = await $.get("/api/error").then((res) => res.data);
+    console.log("ErrorPage");
+    console.log(response);
+    if (response.redirect.redirect_url)
+      navigate(response.redirect.redirect_url);
+  };
+  useEffect(() => {
+    whatError();
+  }, []);
 
   return (
     <div>
--- 42_frontend/src/pages/ServerErrorPage.tsx
+++ 42_frontend/src/pages/ServerErrorPage.tsx
@@ -1,8 +1,24 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 const ServerErrorPage = () => {
   console.log("server error page");
-
+  useEffect(() => {
+    const preventBack = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      // e.returnValue = '';
+    };
+    const handlePopState = () => {
+      window.history.pushState(null, "", window.location.href);
+    };
+    window.history.pushState(null, "", window.location.href);
+    window.addEventListener("popstate", handlePopState);
+    window.addEventListener("beforeunload", preventBack);
+    return () => {
+      window.removeEventListener("popstate", handlePopState);
+      window.removeEventListener("beforeunload", preventBack);
+    };
+  });
   return (
     <div>
       <Outlet />
